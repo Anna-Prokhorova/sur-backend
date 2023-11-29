fc848e8095fbee77c39c15067da8a40293d2b5b1
Прохорова Анна Вячеславовна <avprokhorova@phoenixit.ru>
2023-11-29 15:45:07 +0300

    fixes

--- modules/team-formation.js
+++ modules/team-formation.js
@@ -49,13 +49,26 @@ teamFormationRoutes.post(
   bodyParser.json(),
   async (req, res) => {
     let targetRequest;
-    const file = getJson("all-requests", "team-formation");
-    const allRequests = JSON.parse(file).requests;
+    const fileTeam1 = getJson("projectTeam1", "team-formation");
+    const fileTeam2 = getJson("projectTeam2", "team-formation");
+    const fileTeam3 = getJson("projectTeam3", "team-formation");
+    const allRequests = [
+      ...JSON.parse(fileTeam1),
+      ...JSON.parse(fileTeam2),
+      ...JSON.parse(fileTeam3),
+    ];
 
-    if (req.body.request.id) {
+    const fileEmployees = getJson("employees", "team-formation");
+    const allEmployees = JSON.parse(fileEmployees);
+    const targetEmployee =
+      allEmployees.find((employee) => {
+        return employee.id === req.body.requestBody.employeeId;
+      }) || null;
+
+    if (req.body.id) {
       targetRequest =
         allRequests.find((request) => {
-          return request.requestId === req.body.request.id;
+          return request.requestId === req.body.id;
         }) || null;
     }
 
@@ -78,20 +91,34 @@ teamFormationRoutes.post(
         requestId: targetRequest
           ? targetRequest.requestId
           : `request-id-${allRequests.length}`,
-        projectId: targetRequest ? targetRequest.projectId : "project-id-0",
-        name: targetRequest ? targetRequest.name : "Олег Олегович Олегов",
-        mail: targetRequest ? targetRequest.mail : "mail",
-        img: targetRequest ? targetRequest.img : "/assets/avatar.svg",
-        projectRoleId: req.body.request.requestBody.projectRoleId,
-        positionId: targetRequest ? targetRequest.positionId : "position-id-0",
-        load: req.body.request.requestBody.load,
-        dateStart: req.body.request.requestBody.dateStart,
-        dateEnd: req.body.request.requestBody.dateEnd,
+        projectId: req.body.requestBody.projectId,
+        name: targetRequest ? targetRequest.name : targetEmployee.name,
+        mail: targetRequest ? targetRequest.mail : targetEmployee.mail,
+        employeeId: targetRequest
+          ? targetRequest.employeeId
+          : targetEmployee.employeeId,
+        employeeNumber: targetRequest
+          ? targetRequest.employeeNumber
+          : targetEmployee.employeeNumber,
+        employeeRating: targetRequest
+          ? targetRequest.employeeRating
+          : targetEmployee.employeeRating,
+        img: targetRequest ? targetRequest.img : targetEmployee.img,
+        projectRoleId: req.body.requestBody.projectRoleId,
+        positionId: targetRequest
+          ? targetRequest.positionId
+          : targetEmployee.positionId,
+        roleNameId: targetRequest
+          ? targetRequest.roleNameId
+          : targetEmployee.roleNameId,
+        load: req.body.requestBody.load,
+        dateStart: req.body.requestBody.dateStart,
+        dateEnd: req.body.requestBody.dateEnd,
         status: "ON_APPROVAL",
-        description: req.body.request.requestBody.description,
-        comment: req.body.request.requestBody.comment,
-        recruitmentId: req.body.request.requestBody.recruitmentId,
-        rating: req.body.request.rating,
+        description: req.body.requestBody.description,
+        comment: req.body.requestBody.comment,
+        recruitmentId: req.body.requestBody.recruitmentId,
+        rating: req.body.rating,
         history: targetRequest
           ? [
               ...targetRequest.history,
@@ -112,17 +139,30 @@ teamFormationRoutes.post(
             ],
       };
 
-      // try {
-      //   fs.writeFileSync(
-      //     path.join(__dirname, `../json/team-formation/all-requests.json`),
-      //     JSON.stringify({ requests: [...allRequests, request] })
-      //   );
-      // } catch (error) {
-      //   console.error("Error processing the request:", error);
-      //   return send404Error(res);
-      // }
+      if (!targetRequest) {
+        try {
+          const fileProjectTeam = getJson(
+            `projectTeam${Number(request.projectId.split("-")[2]) + 1}`,
+            "team-formation"
+          );
+          const ProjectTeam = JSON.parse(fileProjectTeam);
 
-      const json = JSON.stringify({ request: request });
+          fs.writeFileSync(
+            path.join(
+              __dirname,
+              `../json/team-formation/projectTeam${
+                Number(request.projectId.split("-")[2]) + 1
+              }.json`
+            ),
+            JSON.stringify([...ProjectTeam, request])
+          );
+        } catch (error) {
+          console.error("Error processing the request:", error);
+          return send404Error(res);
+        }
+      }
+
+      const json = JSON.stringify(request);
       sendAnswerFast(res, json);
     }
   }
@@ -131,8 +171,32 @@ teamFormationRoutes.post(
 teamFormationRoutes.delete("/api_frontend/requests/delete", (req, res) => {
   const isError = processErrors(res);
   if (!isError) {
+    const fileTeam1 = getJson("projectTeam1", "team-formation");
+    const fileTeam2 = getJson("projectTeam2", "team-formation");
+    const fileTeam3 = getJson("projectTeam3", "team-formation");
+    const allRequests = {
+      projectTeam1: JSON.parse(fileTeam1),
+      projectTeam2: JSON.parse(fileTeam2),
+      projectTeam3: JSON.parse(fileTeam3),
+    };
+    for (let key in allRequests) {
+      const filteredRequests = allRequests[key].filter(
+        (request) => request.requestId !== req.query.id
+      );
+      if (filteredRequests.length !== allRequests[key].length) {
+        try {
+          fs.writeFileSync(
+            path.join(__dirname, `../json/team-formation/${key}.json`),
+            JSON.stringify([...filteredRequests])
+          );
+        } catch (error) {
+          console.error("Error processing the request:", error);
+          return send404Error(res);
+        }
+      }
+    }
     const json = JSON.stringify(req.query);
-    sendAnswer(res, json);
+    sendAnswerFast(res, json);
   }
 });
 
