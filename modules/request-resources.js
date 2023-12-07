e0d31bdbfb2291b5204c8648cd8421d4f4adc4b2
Alexandr Kozachenko <Kozachenko.AA@gazprom-neft.ru>
2023-12-07 12:16:38 +0300

    add gant list

--- modules/request-resources.js
+++ modules/request-resources.js
@@ -7,13 +7,18 @@ const requestResources = express.Router();
 
 const { getJson, sendAnswer } = util;
 
-requestResources.get("/api_frontend/getRequestResources", (req, res) => {
+requestResources.get("/api_frontend/requests/list", (req, res) => {
   const json = getJson("list", "request-resources");
   sendAnswer(res, json);
 });
 
 requestResources.get("/api_frontend/workloads", (req, res) => {
   const json = getJson(`gant${req.query.id}`, "request-resources");
+  sendAnswer(res, json);
+});
+
+requestResources.get("/api_frontend/workload/list", (req, res) => {
+  const json = getJson("workload-list", "request-resources");
   sendAnswer(res, json);
 });
 
